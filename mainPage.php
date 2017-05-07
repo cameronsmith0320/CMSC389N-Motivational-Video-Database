0f5dbd280b78f3851b213db00359db0171e7e0c8
Michael Xu <xumike62695@gmail.com>
2017-05-07 18:34:04 -0400

    Added video player functionality

--- mainPage.php
+++ mainPage.php
@@ -23,41 +23,78 @@
                 <div class="clearfix"></div>
             </header>
             <hr>
-		<iframe id="video" width="420" height="315" src="//www.youtube.com/embed/9B7te184ZpQ?rel=0" frameborder="0" allowfullscreen></iframe>
+		
 		<?php
-		/*
-			require_once "databaseLogin.php";
-			$db_connection = new mysqLi($host, $user, $password, $database);
-			if($db_connection->connect_error) {
-				die($db_connection->connect_error);
-			} else {
+			if(isset($_POST['playlist_selected'])) {
+				session_start();
+				$username = $_SESSION['username'];
+				$playlist_name = $_POST['playlist_selected'];
+				
+				$playlist_id = null;
+				$list_of_video_ids = [];
+				$video_player = '<iframe id="video" width="420" height="315" src="//www.youtube.com/embed/';
+				
+				$host = "localhost";
+				$user = "server";
+				$dbpassword = "terps";
+				$database = "motivationaldb"; // max lengths in form to be updated with actual max lengths from database, if needed
+				$table1 = "playlist";
+				$table2 = "playlist_to_video";
+				
+				$db_connection = new mysqli($host, $user, $dbpassword, $database);
+				if ($db_connection->connect_error) {
+					die($db_connection->connect_error);
+				}
+				
+				$query1 = "select * from playlist where username='$username' and playlist_name='$playlist_name'";
+				$result1 = $db_connection->query($query1);
+				
+				if (!$result1) {
+					die("Database access failed: ".$db_connection->error);
+				}
+				else {
+					$num_rows = $result1->num_rows;
+					if ($num_rows !== 0) {
+						$row = $result1->fetch_array(MYSQLI_ASSOC);
+						$playlist_id = $row['playlist_id'];
+					}
+				}
+				
+				$query2 = "select * from playlist_to_video where playlist_id = $playlist_id";
+				$result2 = $db_connection->query($query2);
+				
+				if (!$result2) {
+					die("Database access failed: ".$db_connection->error);
+				}
+				else {
+					$num_rows = $result2->num_rows;
+					if ($num_rows !== 0) {
+						for($i = 0; $i < $num_rows; $i++){
+							$result2->data_seek($i);
+							$row = $result2->fetch_array(MYSQLI_ASSOC);
+							$video_url = $row['video_url'];
+							$video_id = explode("?v=", $video_url)[1];
+							array_push($list_of_video_ids, $video_id);
+						}
+					}
+				}
+				
+				if(sizeof($list_of_video_ids) >= 2){
+					$video_player .= $list_of_video_ids[0].'?playlist=';
+					for($i = 1; $i < sizeof($list_of_video_ids); $i++) {
+						$video_player .= $list_of_video_ids[$i].',';
+					}
+					$video_player = rtrim($video_player, ',');
+					$video_player .= '"frameborder="0" allowfullscreen>></iframe>';
+					echo $video_player;
+				} else if(sizeof($list_of_video_ids) == 1){
+					$video_player .= $list_of_video_ids[0];
+					$video_player .= '"frameborder="0" allowfullscreen>></iframe>';
+					echo $video_player;
+				} else {
+					echo "<bold>Error: no videos in playlist</bold>";
+				}
 			}
-			
-			$selected = $_POST['selected'];
-			
-
-			$query = "select ".implode(", ", $selected)." from applicants";
-			
-			if($_POST['filter'] != '') {
-				$query .= " where ".$_POST['filter'];
-			}
-			
-			$query .= " order by ".$_POST['sorter'];
-			
-			$result = $db_connection->query($query);
-		*/
-			
-			/*Update profile login information if available*/
-			
-			/*Grab first video on the playlist and embed in iFrame*/
-			
-			/*Have the list of videos on the side*/
-			
-			/*On video click, refresh page*/
-			
-			
-			
-			
 		?>
 	
 	<br><br>
