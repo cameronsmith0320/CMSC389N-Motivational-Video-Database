3906a297dff3e73cc37bceca3c7e404c60e29fc1
Cameron Smith <cameronsmith0320@gmail.com>
2017-05-06 21:01:36 -0400

    Linked to main.css

--- createAccount.php
+++ createAccount.php
@@ -147,7 +147,8 @@ FORM;
                 <meta name="viewport" content="width=device-width, initial-scale=1">
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-                <script src="createAccountValidation.js"></script>
+                <link rel="stylesheet" href="main.css">
+		<script src="createAccountValidation.js"></script>
                 <title>Create Account - Database of Motivational Videos</title>	
             </head>
             
